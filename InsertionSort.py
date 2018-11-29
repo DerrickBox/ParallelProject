07481b91146cfd897974def4b18d92fc60ee88be
Carmen Creswell <ccc11@live.missouristate.edu>
2018-11-28 19:49:34 -0600

    Add multiprocessing boilerplate and import time

--- InsertionSort.py
+++ InsertionSort.py
@@ -4,23 +4,63 @@
 * Insertion Sort 
 """
 
+import time 
+import multiprocessing as mp
+
+numProc = 4 #	Starting number of Processes
+
 # Serial Insertion Sort 
-# Source: http://interactivepython.org/courselib/static/pythonds/SortSearch/TheInsertionSort.html
-def insertionSort(alist):
-   for index in range(1,len(alist)):
+# Slightly modified from: http://interactivepython.org/courselib/static/pythonds/SortSearch/TheInsertionSort.html
+def serialInsertionSort(alist):
+	for index in range(1,len(alist)):
+		currentvalue = alist[index]
+		position = index
 
-     currentvalue = alist[index]
-     position = index
+		while position > 0 and alist[position - 1] > currentvalue:
+			alist[position] = alist[position - 1]
+			position = position - 1
 
-     while position > 0 and alist[position - 1] > currentvalue:
-         alist[position]=alist[position-1]
-         position = position-1
+		alist[position] = currentvalue
+	return alist
+	
+def multiInsertionSort(alist): 
+	for index in range(1,len(alist)):
+		currentvalue = alist[index]
+		position = index
 
-     alist[position]=currentvalue
+		while position > 0 and alist[position - 1] > currentvalue:
+			alist[position] = alist[position - 1]
+			position = position - 1
+
+		alist[position] = currentvalue
+	return alist
 
 def main():
-	alist = [54,26,93,17,77,31,44,55,20]
-	insertionSort(alist)
-	print(alist)
+	startTime = time.time()
+	# Serial Calls
+	alist = [54, 26, 93, 17, 77, 31, 44, 55, 20, 100, -1, 8, 33, 33, 55, 0, 3, 90, 100000, 4444, -33]
+	print("Unsorted:", alist)
 	
-main()
+	serialSortedList = serialInsertionSort(alist)
+	endTime = time.time()
+	totalTime = endTime - startTime
+	
+	print("Serial Sorted:", serialSortedList)
+	print("Execution time: {} seconds".format(totalTime))
+
+	# Multiprocessing Calls
+	processes = []
+	for i in range(numProc):
+		p = mp.Process(target = multiInsertionSort, args = [alist,])
+		processes.append(p)
+	
+	for p in processes: 
+		p.start()
+	for p in processes: 
+		p.join()
+	
+	#print("Multiprocessing Sorted:", multiSortedList)
+		
+	
+if __name__ == "__main__":
+    main()
