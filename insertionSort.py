4c1e14584e78094f7c5d92a02dda1e26e0b7710a
Carmen Creswell <31320516+ccreswell97@users.noreply.github.com>
2018-12-12 16:38:24 -0600

    Add insertionSort.py back to the repo
    i think this is back thanks everyone for your patiencve

---
+++ insertionSort.py
@@ -0,0 +1,58 @@
+"""
+* Carmen Creswell 
+* Group 2
+* Insertion Sort 
+"""
+import time 	
+import multiprocessing as mp
+
+numProc = 4 # Starting number of Processes
+
+def insertionSort(alist):
+	for i in range(1,len(alist)):
+		currentValue = alist[i]
+		position = i
+
+		while position > 0 and alist[position - 1] > currentValue:
+			alist[position] = alist[position - 1]
+			position = position - 1
+
+		alist[position] = currentValue
+	return alist
+	
+def multiInsertionSort(conn, alist):	
+    startTime = time.time()	
+    alist = serialInsertionSort(alist)	
+    endTime = time.time()	
+    print("Insertion Sort Completed")	
+    conn.send([alist, endTime - startTime])	
+    conn.close()
+	
+def main():
+	startTime = time.time()
+	alist = [54, 26, 93, 17, 77, 31, 44, 55, 20, 100, -1, 8, 33, 33, 55, 0, 3, 90, 100000, 4444, -33]
+	print("Unsorted:", alist)	
+		
+	serialSortedList = serialInsertionSort(alist)	
+	endTime = time.time()	
+	totalTime = endTime - startTime	
+		
+	print("Serial Sorted:", serialSortedList)	
+	print("Execution time: {} seconds".format(totalTime))	
+ 	# Multiprocessing Calls	
+	"""	
+    processes = []	
+	for i in range(numProc):	
+		p = mp.Process(target = multiInsertionSort, args = [alist,])	
+		processes.append(p)	
+		
+	for p in processes: 	
+		p.start()	
+	for p in processes: 	
+		p.join()	
+		
+	#print("Multiprocessing Sorted:", multiSortedList)
+	"""
+
+if __name__ == "__main__":
+	main()
