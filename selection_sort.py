3e98574512dd96078f3a2c0951ee30325dbda1b8
jguinn124 <44623156+jguinn124@users.noreply.github.com>
2018-12-12 16:23:43 -0600

    Add files via upload

--- selection_sort.py
+++ selection_sort.py
@@ -13,21 +13,3 @@ def selectionSort(alist):
        temp = alist[fillslot]
        alist[fillslot] = alist[positionOfMax]
        alist[positionOfMax] = temp
-	
-def main():
-	
-
-	alist = [54, 26, 93, 17, 77, 31, 44, 55, 20, 100, -1, 8, 33, 33, 55, 0, 3, 90, 100000, 4444, -33]
-	print("Unsorted:", alist)
-
-	startTime = time.time()
-	selectionSort(alist)
-	endTime = time.time()
-	totalTime = endTime - startTime
-
-	print("Serial Sorted:", alist)
-	print("Execution time: {} seconds".format(totalTime))
-		
-	
-if __name__ == "__main__":
-    main()
